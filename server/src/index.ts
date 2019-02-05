4f85c92e85ab023e3e0de914c891bf623f515e7b
Matej Drobnič <matejdro@gmail.com>
2019-02-05 21:36:38 +0100

    Serve display component from the server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,10 @@
 import Koa from "koa";
-import Router from "koa-router";
+import koaMount from "koa-mount";
+import koaStatic from "koa-static";
+import * as path from "path";
 
 const koa = new Koa();
-const router = new Router();
 
-router.get("/*", async (ctx) => {
-    ctx.body = "Hello World";
-});
-
-koa.use(router.routes());
-
-koa.listen(4000);
+koa
+    .use(koaMount("/display/", koaStatic(path.join(process.cwd(), "../display/build"))))
+    .listen(4000);
