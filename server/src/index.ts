f546d199d11d3d46a2e88e2742e4cf8d9a4d7dba
Matej Drobnič <matejdro@gmail.com>
2019-02-07 21:37:26 +0100

    Remove dummy class from server

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,17 +4,12 @@ import koaMount from "koa-mount";
 import Router from "koa-router";
 import koaStatic from "koa-static";
 import * as path from "path";
-import {AudioState} from "../../common/src/AudioState";
 import {loadConfig} from "./Config";
 
 const config = loadConfig();
 
 const koa = new Koa();
 const router = new Router();
-
-// Dummy class to keep dist folder output
-// noinspection JSUnusedLocalSymbols
-const dummy: AudioState = {playbackUuid: "", url: "", paused: false};
 
 router.get("/musicCollections", async (ctx) => {
     const allItems = fs.readdirSync(config.musicPath);
