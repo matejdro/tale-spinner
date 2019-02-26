22ab71294c2c4352e26259afe1306c0a10f8630c
Matej Drobnič <matej.drobnic@inova.si>
2019-02-26 18:42:49 +0100

    Add new initiative editor UI

--- admin/src/ui/InitiativeCard.tsx
+++ admin/src/ui/InitiativeCard.tsx
@@ -1,16 +1,16 @@
-import {Button, Card, EditableText, Switch} from "@blueprintjs/core";
+import {Button, Card, Switch} from "@blueprintjs/core";
 import {action} from "mobx";
-import {inject} from "mobx-react";
+import {inject, observer} from "mobx-react";
 import * as React from "react";
 import {InitiativeEntry} from "../../../common/src/Initiative";
 import {InitiativeController} from "../logic/InitiativeController";
 
+@observer
 class LocalInitiativeCard extends React.Component<InitiativeCardProps, InitiativeEditorState> {
     private firstTextFieldRef = React.createRef<HTMLInputElement>();
 
     constructor(props: InitiativeCardProps) {
         super(props);
-
         this.state = {
             name: "",
             friendly: false,
@@ -20,59 +20,112 @@ class LocalInitiativeCard extends React.Component<InitiativeCardProps, Initiativ
     }
 
     public render(): React.ReactNode {
+        const {initiative} = this.props.initiativeController;
+
+        const initiativeCells = initiative.entries.map(
+            (entry: InitiativeEntry, index: number) => {
+                let colorClass = "row ";
+                if (index === initiative.selectedIndex) {
+                    colorClass += "is-on-turn";
+                } else if (index % 2 === 1) {
+                    colorClass += "is-striped";
+                }
+
+                return (
+                    <div className={colorClass} key={index}>
+                        <div className="cell">{entry.name}</div>
+                        <div className="cell">{entry.friendly ? "✔" : "❌"}</div>
+                        <div className="cell">{entry.initiative.toFixed(2)}</div>
+                        <div className="cell">
+                            <Button text="✏" onClick={this.editAt(index)} className="emoji-button"/>
+                        </div>
+                        <div className="cell">
+                            <Button text="🗑" onClick={this.deleteAt(index)} className="emoji-button"/>
+                        </div>
+                    </div>);
+            });
+
         return (
-            <Card elevation={1} className="card vertical-flex" style={{width: "300px"}}>
+            <Card elevation={1} className="card vertical-flex initiative-card">
                 <h5>
                     <a href="#">Initiative</a>
                 </h5>
 
-                <input
-                    className="mb-10"
-                    ref={this.firstTextFieldRef}
-                    value={this.state.name}
-                    onKeyDown={this.handleKeyEvent}
-                    placeholder="Test"
-                    onChange={this.handleTextChange("name")}
-                />
+                <div className="horizontal-flex mb-20">
+                    <input
+                        className="mr-10"
+                        ref={this.firstTextFieldRef}
+                        value={this.state.name}
+                        onKeyDown={this.handleKeyEvent}
+                        placeholder="Test"
+                        onChange={this.handleTextChange("name")}
+                    />
 
-                <Switch
-                    className="mb-10"
-                    label="Friendly"
-                    checked={this.state.friendly}
-                    onChange={this.handleSwitchChange("friendly")}
-                />
+                    <Switch
+                        className="mr-10 mb-0"
+                        label="Friendly"
+                        checked={this.state.friendly}
+                        onChange={this.handleSwitchChange("friendly")}
+                    />
 
-                <input
-                    className="mb-20"
-                    placeholder="Initiative"
-                    value={this.state.initiative}
-                    onKeyDown={this.handleKeyEvent}
-                    onChange={this.handleTextChange("initiative")}
-                />
+                    <input
+                        placeholder="Initiative"
+                        type="number"
+                        min="0"
+                        max="100"
+                        step="0.01"
+                        value={this.state.initiative}
+                        onKeyDown={this.handleKeyEvent}
+                        onChange={this.handleTextChange("initiative")}
+                    />
+                </div>
 
-                <EditableText
-                    className="mb-20"
-                    placeholder="Delete Index"
-                    value={this.state.deleteIndex}
-                    onConfirm={this.deleteAtIndex}
-                    onChange={this.handleEditableTextChange("deleteIndex")}
-                    selectAllOnFocus={true}
-                />
+                <div className="table mb-20">
+                    <div className="row table-heading">
+                        <div className="cell">Name</div>
+                        <div className="cell">Friendly</div>
+                        <div className="cell">Initiative</div>
+                        <div className="cell">Edit</div>
+                        <div className="cell">Delete</div>
+                    </div>
 
-                <Button onClick={this.clear} text="Izbriši vse" className="mb-20"/>
+                    {initiativeCells}
+                </div>
 
-                <Button onClick={this.advanceSelector} text="Naslednji"/>
+                <Button onClick={this.clear} text="Clear" className="mb-20"/>
+
+                <Button onClick={this.advanceSelector} text="Next"/>
 
             </Card>
         );
     }
-
-    private handleEditableTextChange = (field: string) => (value: string) => {
-        this.setState({[field]: value} as any);
-    }
-
     private handleTextChange = (field: string) => (event: React.FormEvent<HTMLInputElement>) => {
         this.setState({[field]: event.currentTarget.value} as any);
+    }
+
+    @action
+    private deleteAt = (index: number) => () => {
+        this.props.initiativeController.removeEntry(index);
+    }
+
+    @action
+    private editAt = (index: number) => () => {
+        const initiativeController = this.props.initiativeController;
+        const {name, friendly, initiative} = initiativeController.initiative.entries[index];
+
+        this.setState({
+            name,
+            friendly,
+            initiative: initiative.toString(),
+        });
+
+        initiativeController.removeEntry(index);
+
+        const field = this.firstTextFieldRef.current;
+        if (field != null) {
+            field.focus();
+            field.setSelectionRange(0, field.value.length);
+        }
     }
 
     private handleKeyEvent = (event: React.KeyboardEvent<HTMLElement>) => {
